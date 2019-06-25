adddfdd69f5637d1a3b13efef59a4db4b2557a0a
Thang Man <thang@redcranetech.com>
2019-06-25 18:57:56 +0700

    make code cleaner and measure quay.io api response time

--- vulscan.py
+++ vulscan.py
@@ -1,56 +1,45 @@
 import requests
 import json
+import time
 
 url_temp = "https://quay.io/api/v1/repository/{}/{}/tag/{}/images"
-urls     = []
-resp     = []
-
 scan_url_temp = "https://quay.io/api/v1/repository/{}/{}/image/{}/security?vulnerabilities=true"
-scan_urls     = []
-scan_resp     = []
 
-# List of container image IDs to scan vulnerabilities
-ids  = []
-
-# Load json input file, then form the URLs with the values of the keys <Organisation, Repository, Tag>
+# Load json input file
 with open('input.json') as json_file:  
     data = json.load(json_file)
     for d in data:
-        urls.append(url_temp.format(d.get("Organisation"), d.get("Repository"), d.get("Tag")))
         d['Vulnerabilities'] = []
-    
-    # Make HTTP API requests to https://quay.io/api/v1/repository/<org>/<repo>/tag/<tag>/images
-    for u in urls:
-        resp.append(requests.get(u))
-    
-    # Extract ID of the top-layer image, then store each in the list
-    for r in resp:
-        ids.append(
-            (json.loads(r.text)['images'])[0]['id']
-        )
 
-    # Form the URLs for scanning: https://quay.io/api/v1/repository/<org>/<repo>/image/<image_id>/security?vulnerabilities=true
-    i = 0
-    for d in data:
-        scan_urls.append(scan_url_temp.format(d.get("Organisation"), d.get("Repository"), ids[i]))
-        i = i + 1
-    
-    # Make HTTP API requests to <secscan> endpoint
-    for s in scan_urls:
-        scan_resp.append(requests.get(s))
+        # measure elapsed time for responses from Quay API
+        start = time.time()
 
-    # Filter vulnerable packages in each scanned image
-    y = 0
-    for r in scan_resp:
-        for e in (json.loads(r.text)['data']['Layer']['Features']):
+        # Form the URLs with the values of the keys <Organisation, Repository, Tag>
+        # Make HTTP API requests to https://quay.io/api/v1/repository/<org>/<repo>/tag/<tag>/images
+        resp = requests.get(url_temp.format(d.get("Organisation"), d.get("Repository"), d.get("Tag"))) 
+        id = (json.loads(resp.text)['images'])[0]['id']
+
+        end = time.time()
+        print("The time response for query image IDs from Quay API is in seconds: " + str(end - start))
+
+        start = time.time()
+        # Form the URLs for scanning: https://quay.io/api/v1/repository/<org>/<repo>/image/<image_id>/security?vulnerabilities=true    
+        # Make HTTP API requests to <secscan> endpoint
+        scan_resp = requests.get(scan_url_temp.format(d.get("Organisation"), d.get("Repository"), id))
+
+        end = time.time()
+        print("The time response for query (vulnerable) package details from Quay API is in seconds: " + str(end - start))
+        
+        # Filter vulnerable packages in each scanned image
+        y = 0
+        for e in (json.loads(scan_resp.text)['data']['Layer']['Features']):
             if 'Vulnerabilities' in e:
                 for v in e['Vulnerabilities']:
                     v['PackageName'] = e['Name']
                     data[y]['Vulnerabilities'].append(v)
         y = y + 1
-    
+
     # Write a json output file to current directory
     with open('result.json', 'w') as outfile:  
         json.dump(data, outfile, indent=4)
-
 
